94201e12242304cd56fc6d220440f1f32355f4fc
hiwex-dev <hiwexenterprise@gmail.com>
2020-07-13 02:15:28 -0600

    INIT

--- app/views/login/index.js
+++ app/views/login/index.js
@@ -2,10 +2,12 @@
 import React, { Component } from 'react';
 import {
 	View, TextInput, TouchableOpacity,
-	Text, StyleSheet
+	Text, StyleSheet, Image,
 } from 'react-native';
 
 import auth from '@react-native-firebase/auth';
+
+import userImg from '../../assets/icons/usuario.png'
 
 export const styles = StyleSheet.create({
 	container: {
@@ -16,6 +18,8 @@ export const styles = StyleSheet.create({
 	},
 	subcontainer: {
 		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
 	},
 	title: {
 		color: '#FFF',
@@ -40,45 +44,74 @@ export const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	img: {
+		width: 100,
+		height: 100,
+		tintColor: '#FFF',
+	},
 });
 
 class Login extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			Email: null,
+			Password: null,
+		};
+	}
 	componentDidMount() {
-		auth()
-			.createUserWithEmailAndPassword('sarah.lane@gmail.com', 'SuperSecretPassword!')
-			.then(() => {
-				console.log('User account created & signed in!');
-			})
-			.catch(error => {
-				if (error.code === 'auth/email-already-in-use') {
-				console.log('That email address is already in use!');
-				}
+		// auth()
+		// 	.createUserWithEmailAndPassword('sarah.lane@gmail.com', 'SuperSecretPassword!')
+		// 	.then(() => {
+		// 		console.log('User account created & signed in!');
+		// 	})
+		// 	.catch(error => {
+		// 		if (error.code === 'auth/email-already-in-use') {
+		// 		console.log('That email address is already in use!');
+		// 		}
 
-				if (error.code === 'auth/invalid-email') {
-				console.log('That email address is invalid!');
-				}
+		// 		if (error.code === 'auth/invalid-email') {
+		// 		console.log('That email address is invalid!');
+		// 		}
 
-				console.error(error);
-			});
+		// 		console.error(error);
+		// 	});
 	}
 
 	render() {
+		const { Email, Password } = this.state;
 		return (
 			<View style={styles.container}>
-				<View style={styles.subcontainer} />
+				<View style={styles.subcontainer}>
+					<Image
+						source={userImg}
+						style={styles.img}
+					/>
+				</View>
 
 				<View style={styles.subcontainer}>
 					<Text style={styles.title}>Email</Text>
 					<TextInput
 						style={styles.text}
+						value={Email}
+						onChangeText={em => this.setState({ Email: em })}
 					/>
 					<Text style={styles.title}>Password</Text>
 					<TextInput
 						style={styles.text}
+						value={Password}
+						onChangeText={psw => this.setState({ Password: psw })}
+						secureTextEntry
 					/>
 
 					<TouchableOpacity
 						style={styles.btn}
+						onPress={() => {
+							auth().signInWithEmailAndPassword(Email, Password)
+								.then(usr => console.log({ usr }))
+								.catch(err => console.log({ err }));
+						}}
 					>
 						<Text style={styles.title}>Login</Text>
 					</TouchableOpacity>
