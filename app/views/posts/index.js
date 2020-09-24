4e4a2c2f96ec0453f38890fb6e4319a376f0dd83
HWEX DEV <hwexdev@HWEXs-MacBook-Pro.local>
2020-09-24 18:43:21 -0500

    INIT

--- app/views/posts/index.js
+++ app/views/posts/index.js
@@ -1,7 +1,7 @@
 // Dependencies
 import React, { useEffect, useState } from 'react';
 import {
-	View, ScrollView,
+	View, FlatList, Text
 } from 'react-native';
 
 // Components
@@ -12,33 +12,55 @@ import { styles } from './styles';
 
 
 const Posts = () => {
-    const arr = [
-        {name: 'name 1'},
-        {name: 'name 2'},
-        {name: 'name 3'},
-        {name: 'name 4'},
-        {name: 'name 5'},
-        {name: 'name 6'},
-        {name: 'name 7'},
-        {name: 'name 8'},
-        {name: 'name 9'},
-        {name: 'name 10'},
-        {name: 'name 11'},
-        {name: 'name 12'},
-    ]
+    const [arr, setArr] = useState([]);
+    const [data, setData] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const [refreshing, setRefresh] = useState(false);
+
+    const getData = () => {
+        const end = counter + 30;
+        const slice = data.slice(counter, end);
+        setArr([...arr, ...slice]);
+        setCounter(end);
+    }
+
+    useEffect(() => {
+        const _arr = [];
+        for (let index = 0; index < 100; index++) {
+            _arr.push({ name: `name ${index}` });
+        }
+
+        setData(_arr);
+        const end = counter + 30;
+        const slice = _arr.slice(counter, end);
+        setArr(slice);
+        setCounter(end);
+    }, [])
+
     return (
         <View style={styles.container}>
-            <ScrollView>
-                {
-                    arr.map(i => (
-                        <Button
-                            title={i.name}
-                            action={() => console.log(i.name)}
-                        />
-                    ))
-                }
-            </ScrollView>
-           
+            <FlatList
+                data={arr}
+                renderItem={({ item, index }) => (
+                    <Button title={item.name} action={() => console.log(item.name)}/>
+                )}
+                showsVerticalScrollIndicator={false}
+                ItemSeparatorComponent={() => (
+                    <View style={{ height: 10, width: '100%', backgroundColor: 'black' }}/>
+                )}
+                ListEmptyComponent={() => (
+                    <View style={{ flex: 1, marginTop: 30 }}>
+                        <Text>Empty</Text>
+                    </View>
+                )}
+                onEndReached={getData}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setArr([]);
+                    setCounter(0);
+                    console.log('Hola');
+                }}
+            />
         </View>
     );
 }
