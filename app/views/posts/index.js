9893b4cabf2c5e0b861d23d4422cb98a66cbd486
HWEX DEV <hwexdev@HWEXs-MacBook-Pro.local>
2020-11-12 18:08:13 -0600

    INIT

--- app/views/posts/index.js
+++ app/views/posts/index.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { View, TextInput } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+import Crypto from 'crypto-js';
 
 // Components
 import Button from '../../components/button';
@@ -28,11 +29,38 @@ const Posts = () => {
 						cancelButton: 'Cancelar',
 						takePhotoButtonTitle: 'Tomar Foto',
 						chooseFromLibraryButtonTitle: 'Abrir Galeria',
-						noData: true,
+                        noData: true,
+                        quality: 0
 					};
 
 					ImagePicker.showImagePicker(options, (res) => {
-						console.log({ res });
+                        
+                        const uri = res.uri;
+                        const type = res.type;
+                        const name = res.fileName || 'image.jpg';
+
+                        const photo = { uri, type, name };
+                        const ts = Math.round((new Date()).getTime() / 1000);
+                        const apiKey = '732334713499588';
+                        const apiSecret = 'yA6Oo9zv2x_K__i7lO0C-Fiy0l4';
+                        const hash = `timestamp=${ts}${apiSecret}`;
+                        const signature = Crypto.SHA1(hash).toString();
+                        const url = 'https://api.cloudinary.com/v1_1/test-hiwex/image/upload';
+
+                        const formData = new FormData();
+                        formData.append('file', photo);
+                        formData.append('timestamp', ts);
+                        formData.append('api_key', apiKey);
+                        formData.append('signature', signature);
+
+                        fetch(url, {
+                            method: 'POST',
+                            body: formData,
+                        })
+                        .then(res => res.json())
+                        .then(res => console.log({ res }))
+                        .catch(err => console.log({ err }))
+
 					});
 				}}
             />
